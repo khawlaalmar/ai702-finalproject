5a88a47bea296d89b77530ff4b7b5d8c9d0d7e53
Nazira Dunbayeva <nazira.dunbayeva@mbzuai.ac.ae>
2025-04-30 15:15:33 +0400

    Update clip+sla.py

--- scripts/clip+sla.py
+++ scripts/clip+sla.py
@@ -1,9 +1,9 @@
 import json
 
 # === Paths ===
-clip_path = "/home/amal.saqib/Downloads/CV703_FoodGen_Project/finetuning_experiments/experiment_5_FoodCap_6decoder/captions_finetuned_clip_scores.json"
-sla_path = "/home/amal.saqib/Downloads/CV703_FoodGen_Project/finetuning_experiments/experiment_5_FoodCap_6decoder/captions_finetuned_sla_scores_from_embeddings.json"
-output_path = "/home/amal.saqib/Downloads/CV703_FoodGen_Project/finetuning_experiments/experiment_5_FoodCap_6decoder/captions_finetuned_combined_clip_sla_scores.json"
+clip_path = "/home/Downloads/CV703_FoodGen_Project/finetuning_experiments/experiment_5_FoodCap_6decoder/captions_finetuned_clip_scores.json"
+sla_path = "/home/Downloads/CV703_FoodGen_Project/finetuning_experiments/experiment_5_FoodCap_6decoder/captions_finetuned_sla_scores_from_embeddings.json"
+output_path = "/home/Downloads/CV703_FoodGen_Project/finetuning_experiments/experiment_5_FoodCap_6decoder/captions_finetuned_combined_clip_sla_scores.json"
 
 # === Load both files ===
 with open(clip_path, "r") as f:
